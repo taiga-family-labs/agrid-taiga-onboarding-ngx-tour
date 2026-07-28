959cd74168de485e15189188bff5a3b5feac76fe
Максим Иванов <omaxphp@yandex.ru>
2026-07-28 16:22:26 +0300

    refactor: adapt grid cell to travel plans

--- src/app/grid/employee-cell.component.ts
+++ src/app/grid/employee-cell.component.ts
@@ -4,31 +4,31 @@ import {ICellRendererAngularComp} from 'ag-grid-angular';
 import {ICellRendererParams} from 'ag-grid-community';
 import {TourAnchorTuiHintDirective} from 'ngx-ui-tour-tui-hint';
 
-import {COMMENT_TOUR} from '../comments/comment-tour/comment-tour.provider';
-import {COMMENT_TOUR_ANCHORS} from '../comments/comment-tour/comment-tour.service';
-import {CommentsSidebarService} from '../comments/comments-sidebar/comments-sidebar.service';
-import {ProposalDto} from '../proposal.dto';
+import {TRAVEL_NOTES_TOUR} from '../comments/comment-tour/comment-tour.provider';
+import {TRAVEL_NOTES_TOUR_ANCHORS} from '../comments/comment-tour/comment-tour.service';
+import {TravelNotesSidebarService} from '../comments/comments-sidebar/comments-sidebar.service';
+import {TravelPlanDto} from '../proposal.dto';
 
 @Component({
-    selector: 'employee-cell',
+    selector: 'travel-plan-cell',
     imports: [TourAnchorTuiHintDirective, TuiButton],
     templateUrl: './employee-cell.component.html',
     styleUrl: './employee-cell.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EmployeeCellComponent implements ICellRendererAngularComp {
-    protected readonly sidebar = inject(CommentsSidebarService);
-    protected readonly commentTour = inject(COMMENT_TOUR);
-    protected readonly anchors = COMMENT_TOUR_ANCHORS;
-    protected proposal!: ProposalDto;
+export class TravelPlanCellComponent implements ICellRendererAngularComp {
+    protected readonly sidebar = inject(TravelNotesSidebarService);
+    protected readonly travelNotesTour = inject(TRAVEL_NOTES_TOUR);
+    protected readonly anchors = TRAVEL_NOTES_TOUR_ANCHORS;
+    protected travelPlan!: TravelPlanDto;
 
-    public agInit(params: ICellRendererParams<ProposalDto>): void {
+    public agInit(params: ICellRendererParams<TravelPlanDto>): void {
         if (params.data) {
-            this.proposal = params.data;
+            this.travelPlan = params.data;
         }
     }
 
-    public refresh(params: ICellRendererParams<ProposalDto>): boolean {
+    public refresh(params: ICellRendererParams<TravelPlanDto>): boolean {
         this.agInit(params);
 
         return true;
