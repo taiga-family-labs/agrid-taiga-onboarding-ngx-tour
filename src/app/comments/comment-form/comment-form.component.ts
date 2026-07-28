0bee7e5b592a836177f1939207d7d8e7571e098f
Максим Иванов <omaxphp@yandex.ru>
2026-07-28 16:21:53 +0300

    refactor: adapt note form to travel planning

--- src/app/comments/comment-form/comment-form.component.ts
+++ src/app/comments/comment-form/comment-form.component.ts
@@ -3,34 +3,32 @@ import {FormsModule} from '@angular/forms';
 import {TuiButton, TuiIcon} from '@taiga-ui/core';
 import {TourAnchorTuiHintDirective} from 'ngx-ui-tour-tui-hint';
 
-import {ProposalDto} from '../../proposal.dto';
-import {
-    COMMENT_TOUR,
-} from '../comment-tour/comment-tour.provider';
-import {COMMENT_TOUR_ANCHORS} from '../comment-tour/comment-tour.service';
+import {TravelPlanDto} from '../../proposal.dto';
+import {TRAVEL_NOTES_TOUR} from '../comment-tour/comment-tour.provider';
+import {TRAVEL_NOTES_TOUR_ANCHORS} from '../comment-tour/comment-tour.service';
 
 @Component({
-    selector: 'comment-form',
+    selector: 'travel-note-form',
     imports: [FormsModule, TourAnchorTuiHintDirective, TuiButton, TuiIcon],
     templateUrl: './comment-form.component.html',
     styleUrl: './comment-form.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CommentFormComponent {
-    protected readonly commentTour = inject(COMMENT_TOUR);
-    protected readonly anchors = COMMENT_TOUR_ANCHORS;
-    protected comment = '';
-    protected isJustification = false;
+export class TravelNoteFormComponent {
+    protected readonly travelNotesTour = inject(TRAVEL_NOTES_TOUR);
+    protected readonly anchors = TRAVEL_NOTES_TOUR_ANCHORS;
+    protected note = '';
+    protected addToChecklist = false;
 
-    public readonly proposal = input.required<ProposalDto>();
+    public readonly travelPlan = input.required<TravelPlanDto>();
 
     protected cancel(): void {
-        this.comment = '';
-        this.isJustification = false;
+        this.note = '';
+        this.addToChecklist = false;
     }
 
     protected submit(): void {
-        this.comment = '';
-        this.isJustification = false;
+        this.note = '';
+        this.addToChecklist = false;
     }
 }
